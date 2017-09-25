fd55caeeca58e4422e4e38a756dfca1a06e1804a
Kojo Hnson <kojohnson@Kojos-MacBook-Pro.local>
2017-09-25 13:31:33 +0100

    Refactor to use operation objects instead of Errors

--- matrix-lib/execPipe.js
+++ matrix-lib/execPipe.js
@@ -1,38 +1,42 @@
+const {
+    Failure
+} = require('./utils')
+
 const execPipe = fnlist => {
     return async(action) => {
-        if (!action) throw Error('Worker requires action object on request body to perform operations on... duh!')
+        if (!action) return new Failure('Worker requires action object on request body to perform operations on... duh!')
         const numberOfFns = fnlist.length
+        if (!numberOfFns || numberOfFns < 1) throw Error('Fn array needs at least 1 fn ...dudh!')
         let i = numberOfFns - 1
         let dto = action
         for (i; i >= 0; i--) {
             if (fnlist[i].constructor.name === 'Function') {
                 const fn = fnlist[i]
-                const operation = fn(dto)
+                try {
+                    const operation = fn(dto)
 
-                if (!operation || operation.outcome === undefined) return {
-                    failed: true,
-                    details: 'No operation outcome was specfifed!',
-                    outcome: null
+                    if (!operation) return new Failure('No operation outcome was specfifed!')
+                    if (operation.failed === undefined || operation.outcome === undefined) return new Failure('Invalid operation outcome')
+                    if (operation.failed) return new Failure(operation.details)
+
+                    dto = operation.outcome
+                } catch (error) {
+                    return new Failure(error.message)
                 }
-
-                if (operation.failed) throw Error(operation.details)
-
-                dto = operation.outcome
             } else if (fnlist[i].constructor.name === 'AsyncFunction') {
                 const fnAsync = fnlist[i]
                 try {
                     const operation = await fnAsync(dto)
 
-                    if (!operation || operation.outcome === undefined) return {
-                        failed: true,
-                        details: 'No operation outcome was specfifed!',
-                        outcome: null
-                    }
+                    if (!operation) return new Failure('No operation outcome was specfifed!')
+                    if (operation.failed === undefined || operation.outcome === undefined) return new Failure('Invalid operation outcome')
+                    if (operation.failed) throw Error(operation.details)
 
+                    dto = operation.outcome
                 } catch (error) {
-                    throw Error(error)
+                    return new Failure(error.message)
                 }
-            } else throw Error('Not a fn!')
+            } else return new Failure('Not a fn!')
         }
         return dto
     }
